8e8f19261da559cdfaca815e171a77ecec48686b
daniele <danivilarez@hotmail.com>
2021-08-31 22:08:40 -0300

    atividade keila

---
+++ exercicio.js
@@ -0,0 +1,18 @@
+var letra = prompt ("insira uma letra");
+
+
+
+switch (letra) {
+    case 'a':alert('A letra inserida é uma vogal')
+        break;
+    case 'e':alert('A letra inserida é uma vogal')
+        break;
+    case 'i':alert('A letra inserida é uma vogal')
+        break;
+     case 'o':alert('A letra inserida é uma vogal')
+        break;
+     case 'u':alert('A letra inserida é uma vogal')
+        break;
+    default:alert ('A letra inserida é uma consoante')
+    }
+
---
+++ farmacia.js
@@ -0,0 +1,21 @@
+var produto1 = Number(prompt("Informe o valor do produto"));
+var produto2 = Number(prompt("Informe o valor do produto"));
+
+
+if(produto1==produto2){  
+
+
+var desconto =(produto1 + produto2) -5;
+
+alert("Seu total a pagar com desconto é " + desconto);
+
+
+}
+
+else{
+
+    var desconto2 = produto1 + produto2;
+ alert("Seu total a pagar é "  + desconto2);
+
+}
+
